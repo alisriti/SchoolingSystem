1d17144387c3b73f03de3f7f5347e30d86a84e9a
ali sriti <alisriti@live.fr>
2022-02-22 22:46:39 +0000

    Etudiant : Create Model

---
+++ Models/Etudiants/Etudiant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SchoolingSystem.Models.Etudiants
+{
+    public class Etudiant
+    {
+        public string ID { get; set; }
+        public string NumCarte { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DDN { get; set; }
+        public string LDN { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+    }
+}
