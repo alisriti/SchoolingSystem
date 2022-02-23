d275f1ba3184464d04ee732619fca4a5675413b0
ali sriti <alisriti@live.fr>
2022-02-23 21:33:23 +0000

    Etudiants Card list

---
+++ Views/Components/Etudiants/EtudiantsCardList.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using SchoolingSystem.Models.Etudiants;
+using System.Collections.Generic;
+
+namespace SchoolingSystem.Views.Components.Etudiants
+{
+    public partial class EtudiantsCardList
+    {
+        [Parameter] public List<Etudiant> Etudiants { get; set; }
+    }
+}
