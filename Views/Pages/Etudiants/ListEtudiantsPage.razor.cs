7bf7e34d99f173e8bd2e73e92fde7f698df743ea
ali sriti <alisriti@live.fr>
2022-02-23 21:56:58 +0000

    Switching between List and Cards in ListEtudiants page

--- Views/Pages/Etudiants/ListEtudiantsPage.razor.cs
+++ Views/Pages/Etudiants/ListEtudiantsPage.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using SchoolingSystem.Managers.Storages.Etudiants;
 using SchoolingSystem.Models.Etudiants;
+using SchoolingSystem.Views.Models;
 using System.Collections.Generic;
 
 namespace SchoolingSystem.Views.Pages.Etudiants
@@ -10,10 +11,15 @@ namespace SchoolingSystem.Views.Pages.Etudiants
         [Inject] private IEtudiantStore etudiantStore { get; set; }
 
         private List<Etudiant> etudiants;
+        private ListViewMode viewMode = ListViewMode.Table;
 
         protected override void OnInitialized()
         {
             etudiants = etudiantStore.SelectEtudiants();
         }
+
+        private void switchToTable() => viewMode = ListViewMode.Table;
+
+        private void switchToCards() => viewMode = ListViewMode.Cards;
     }
 }
