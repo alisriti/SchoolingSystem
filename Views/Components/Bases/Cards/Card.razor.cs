dd6b696d9239f5bd1687fe387bd54eee1cb71c20
ali sriti <alisriti@live.fr>
2022-02-23 21:31:31 +0000

    Base Card Component

---
+++ Views/Components/Bases/Cards/Card.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SchoolingSystem.Views.Components.Bases.Cards
+{
+    public partial class Card
+    {
+        [Parameter] public string Width { get; set; } = "300px";
+        [Parameter] public RenderFragment CardHeader { get; set; }
+        [Parameter] public RenderFragment CardBody { get; set; }
+        [Parameter] public RenderFragment CardFooter { get; set; }
+    }
+}
