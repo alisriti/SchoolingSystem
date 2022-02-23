abeddb7089554adfede5aeaf6d40aa46b91334cd
ali sriti <alisriti@live.fr>
2022-02-23 21:13:51 +0000

    List Etudiants Page

---
+++ Views/Pages/Etudiants/ListEtudiantsPage.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using SchoolingSystem.Managers.Storages.Etudiants;
+using SchoolingSystem.Models.Etudiants;
+using System.Collections.Generic;
+
+namespace SchoolingSystem.Views.Pages.Etudiants
+{
+    public partial class ListEtudiantsPage
+    {
+        [Inject] private IEtudiantStore etudiantStore { get; set; }
+
+        private List<Etudiant> etudiants;
+
+        protected override void OnInitialized()
+        {
+            etudiants = etudiantStore.SelectEtudiants();
+        }
+    }
+}
