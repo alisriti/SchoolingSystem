964ef068cb15dc4e49f15f0029027f9d6c8d0ed3
ali sriti <alisriti@live.fr>
2022-02-23 21:19:04 +0000

    Create component to display list of Etudiants

---
+++ Views/Components/Etudiants/EtudiantTableList.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using SchoolingSystem.Models.Etudiants;
+using System.Collections.Generic;
+
+namespace SchoolingSystem.Views.Components.Etudiants
+{
+    public partial class EtudiantTableList
+    {
+        [Parameter] public List<Etudiant> Etudiants { get; set; }
+    }
+}
