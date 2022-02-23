8109864bc30969228a7582d1abf9e22f4d5b0938
ali sriti <alisriti@live.fr>
2022-02-23 21:21:18 +0000

    Create IF Component

---
+++ Views/Components/Bases/IF.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SchoolingSystem.Views.Components.Bases
+{
+    public partial class IF
+    {
+        [Parameter] public bool Condition { get; set; }
+        [Parameter] public RenderFragment Then { get; set; }
+        [Parameter] public RenderFragment Else { get; set; }
+    }
+}
