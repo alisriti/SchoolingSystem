f618dbacd61b8f898c29094013f21b3a75a8c359
ali sriti <alisriti@live.fr>
2022-02-23 21:31:42 +0000

    Etudiant Card

---
+++ Views/Components/Etudiants/EtudiantCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using SchoolingSystem.Models.Etudiants;
+
+namespace SchoolingSystem.Views.Components.Etudiants
+{
+    public partial class EtudiantCard
+    {
+        [Parameter] public Etudiant Etudiant { get; set; }
+    }
+}
